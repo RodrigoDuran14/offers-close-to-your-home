8925f3aaee994548d6746a90dad28b49e5f01579
jesusudiz <jesusgabrieludiz@gmail.com>
2023-04-01 02:25:58 -0300

    se realizó el codigo de redux relacionado con los productos

--- client_users/src/redux/actions.js
+++ client_users/src/redux/actions.js
@@ -1,16 +1,55 @@
 import axios from "axios";
 
-import { CREATE_PRODUCT } from './actions-type'
+import * as action from './actions-type' // para no escribir todos los action types los obtuve todos con el uso del * y lo renombre como action...para usar colocar la palabra action.[nombre del action-type]
 
-const BACK_HOST = 'http://localhost:3001'
+const URL = 'http://localhost:3001'
 
 // ========================* PRODUCTS *========================
 export function createProduct(product) {
-    console.log(`createProduct function (${product})`)
+    return async (dispatch) => {
+        try {
+          const response = await axios.post(`${URL}/products`, product);
+          console.log(response.data)
+          dispatch({ type: action.CREATE_PRODUCT, payload: response.data });
+        } catch (error) {
+          console.log(error);
+          dispatch({ type: action.CREATE_PRODUCT, payload: error});
 
-    return () => {
-        axios.post(`${BACK_HOST}/products`, product)
-            .then(response => console.log(response))
-            .catch(err => console.log(err))
-    }
+        }
+      };
 }
+
+// * 2. action-creator para obtener todos los productos del back-end 
+
+export const getAllProducts = ()=>{
+    return async (dispatch) => {
+        try {
+          const response = await axios.get(`${URL}/products`);
+          console.log(response.data)
+          dispatch({ type: action.GET_ALL_PRODUCTS, payload: response.data });
+        } catch (error) {
+          console.log(error);
+          dispatch({ type: action.GET_ALL_PRODUCTS, payload: error});
+        }
+      };
+}
+
+// * 3.action-creator para obtener producto por ID
+
+
+export const getProductById = (id) => async dispatch => {
+    try {
+      const res = await axios.get(`${URL}/products/${id}`)
+  
+      dispatch({
+        type: action.GET_PRODUCT_BY_ID,
+        payload: res.data
+      })
+    } catch (error) {
+        console.log(error);
+        dispatch({
+            type: action.GET_PRODUCT_BY_ID,
+            payload: error
+          })
+    };
+  };
