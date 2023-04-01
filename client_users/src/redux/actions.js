96c3e20d7761c6c4b7fcc3eb462fee370a3bd813
jesusudiz <jesusgabrieludiz@gmail.com>
2023-04-01 13:26:09 -0300

    update de lo trabajado en redux en mi rama

--- client_users/src/redux/actions.js
+++ client_users/src/redux/actions.js
@@ -8,30 +8,30 @@ const URL = 'http://localhost:3001'
 export function createProduct(product) {
     return async (dispatch) => {
         try {
-          const response = await axios.post(`${URL}/products`, product);
-          console.log(response.data)
-          dispatch({ type: action.CREATE_PRODUCT, payload: response.data });
+            const response = await axios.post(`${URL}/products`, product);
+            console.log(response.data)
+            dispatch({ type: action.CREATE_PRODUCT, payload: response.data });
         } catch (error) {
-          console.log(error);
-          dispatch({ type: action.CREATE_PRODUCT, payload: error});
+            console.log(error);
+            dispatch({ type: action.CREATE_PRODUCT, payload: error });
 
         }
-      };
+    };
 }
 
 // * 2. action-creator para obtener todos los productos del back-end 
 
-export const getAllProducts = ()=>{
+export const getAllProducts = () => {
     return async (dispatch) => {
         try {
-          const response = await axios.get(`${URL}/products`);
-          console.log(response.data)
-          dispatch({ type: action.GET_ALL_PRODUCTS, payload: response.data });
+            const response = await axios.get(`${URL}/products`);
+            console.log(response.data)
+            dispatch({ type: action.GET_ALL_PRODUCTS, payload: response.data });
         } catch (error) {
-          console.log(error);
-          dispatch({ type: action.GET_ALL_PRODUCTS, payload: error});
+            console.log(error);
+            dispatch({ type: action.GET_ALL_PRODUCTS, payload: error });
         }
-      };
+    };
 }
 
 // * 3.action-creator para obtener producto por ID
@@ -39,17 +39,96 @@ export const getAllProducts = ()=>{
 
 export const getProductById = (id) => async dispatch => {
     try {
-      const res = await axios.get(`${URL}/products/${id}`)
-  
-      dispatch({
-        type: action.GET_PRODUCT_BY_ID,
-        payload: res.data
-      })
+        const res = await axios.get(`${URL}/products/${id}`)
+
+        dispatch({
+            type: action.GET_PRODUCT_BY_ID,
+            payload: res.data
+        })
     } catch (error) {
         console.log(error);
         dispatch({
             type: action.GET_PRODUCT_BY_ID,
             payload: error
-          })
+        })
     };
-  };
+};
+
+// * 4.action-creator para obtener producto por nombre
+
+export const getProductByName = (name) => async (dispatch) => {
+    try {
+        const res = await axios.get(`${URL}/products}`)
+        const result = res.data.filter(product => product.nombre === name)
+
+        dispatch({
+            type: action.GET_PRODUCT_BY_NAME,
+            payload: result
+        })
+    } catch (error) {
+        console.log(error);
+        dispatch({
+            type: action.GET_PRODUCT_BY_NAME,
+            payload: error
+        })
+    };
+
+}
+
+// * 5. action-creator para obtener producto por categoría
+
+export const getProductByCategory = (name) => async (dispatch) => {
+    try {
+        const res = await axios.get(`${URL}/products}`)
+        const result = res.data.filter(product => product.nombre === name)
+
+        dispatch({
+            type: action.GET_PRODUCT_BY_CATEGORY,
+            payload: result
+        })
+    } catch (error) {
+        console.log(error);
+        dispatch({
+            type: action.GET_PRODUCT_BY_CATEGORY,
+            payload: error
+        })
+    };
+
+}
+
+// * 6. action-creator para ordenar productos por nombre ascendente
+
+export const orderedByNameASC = () => {
+    return { type: action.ORDERED_BY_NAME_ASC }
+}
+
+// * 7. action-creator para ordenar productos por nombre descendente
+export const orderedByNameDESC = () => {
+    return { type: action.ORDERED_BY_NAME_DESC }
+}
+
+// * 8. action-creator para ordenar productos por menor precio
+
+export const orderedByLowestPrice = () => {
+    return { type: action.ORDERED_BY_LOWEST_PRICE }
+}
+
+// * 9. action-creator para ordenar productos por mayor precio
+
+export const orderedByHighestPrice = () => {
+    return { type: action.ORDERED_BY_HIGHEST_PRICE }
+}
+
+// * 10. action-creator para filtrar productos por condicion (Nuevo,Usado,Reacondicionado)
+
+export const filterByNewProducts = () => {
+    return { type: action.FILTER_BY_NEW_PRODUCTS };//productos nuevos
+};
+
+export const filterByUsedProducts = () => {
+    return { type: action.FILTER_BY_USED_PRODUCTS };//productos usados
+};
+
+export const filterByRefurbishedProducts = () => {
+    return { type: action.FILTER_BY_REFURBISHED_PRODUCTS };//productos reacondicionados
+};
--- client_users/src/redux/store.js
+++ client_users/src/redux/store.js
@@ -4,9 +4,12 @@ import rootReducer from './reducer'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 
-export const store = createStore(
+const store = createStore(
     rootReducer,
     composeWithDevTools(
       applyMiddleware(thunk)
     )
   );
+
+
+export default store;
