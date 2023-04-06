04d8314cdc73663016e470b96c1b28e734ae8506
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-04-06 17:25:00 -0300

    Arreglo postComercio

--- api/src/handlers/comercios/postComercio.js
+++ api/src/handlers/comercios/postComercio.js
@@ -1,39 +1,18 @@
-const { createCommerce } = require("../../controllers/comercios/postComercioController");
-const { searchNameCommerce, searchEmailCommerce } = require("../../controllers/comercios/getComercioController");
-const { validacionPostComercio } = require("../validaciones/validacionComercio");
+const {
+  createCommerce,
+} = require("../../controllers/comercios/postComercioController");
+const {
+  searchNameCommerce,
+  searchEmailCommerce,
+} = require("../../controllers/comercios/getComercioController");
+const {
+  validacionPostComercio,
+} = require("../validaciones/validacionComercio");
 
 const postCommerceHandler = async (req, res) => {
-
-    const {id_ciudad, id_categoria_comercio, nombre_comercio, direccion, telefono, estado, nombre_contacto, cargo, password, email, imagen } = req.body
-
-    try {
-        if ( nombre_comercio, direccion, telefono, estado, nombre_contacto, cargo, password, email, imagen) {
-
-            const [resultSearchName, resultSearchEmail] = await Promise.all([
-                searchNameCommerce(nombre_comercio),
-                searchEmailCommerce(email)
-            ])
-            if (resultSearchEmail === null && resultSearchName === null) {
-
-                const newCommerce = await createCommerce(id_ciudad, id_categoria_comercio, nombre_comercio, direccion, telefono, estado, nombre_contacto, cargo, password, email, imagen)
-                res.status(200).json(newCommerce)
-            }
-            else if (resultSearchEmail !== null) {
-                res.status(300).send({ data: "ya existe un camercio con ese email" })
-            }
-            else {
-                res.status(300).send({ data: "ya existe un camercio con ese nambre" })
-            }
-        }
-
-        else { res.status(300).send({ data: "faltan llenar campos" }) }
-
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-
   const {
-    id_categoria_comercio,
     id_ciudad,
+    id_categoria_comercio,
     nombre_comercio,
     direccion,
     telefono,
@@ -46,37 +25,49 @@ const postCommerceHandler = async (req, res) => {
   } = req.body;
 
   try {
-    //validacionPostComercio(req.body);
-
-  const [resultSearchName, resultSearchEmail] = await Promise.all([
-    searchNameCommerce(nombre_comercio),
-    searchEmailCommerce(email),
-  ]);
-  if (resultSearchEmail === null && resultSearchName === null) {
-      const newCommerce = await createCommerce(
-        id_categoria_comercio,
-        id_ciudad,
-        nombre_comercio,
-        direccion,
-        telefono,
-        estado,
-        nombre_contacto,
-        cargo,
-        password,
-        email,
-        imagen
-      );
-      res.status(200).json(newCommerce);
-    } else if (resultSearchEmail !== null) {
-      res.status(300).send({ data: "ya existe un camercio con ese email" });
+    if (
+      nombre_comercio &&
+      direccion &&
+      telefono &&
+      estado &&
+      nombre_contacto &&
+      cargo &&
+      password &&
+      email &&
+      imagen
+    ) {
+      const [resultSearchName, resultSearchEmail] = await Promise.all([
+        searchNameCommerce(nombre_comercio),
+        searchEmailCommerce(email),
+      ]);
+      if (resultSearchEmail === null && resultSearchName === null) {
+        const newCommerce = await createCommerce(
+          id_ciudad,
+          id_categoria_comercio,
+          nombre_comercio,
+          direccion,
+          telefono,
+          estado,
+          nombre_contacto,
+          cargo,
+          password,
+          email,
+          imagen
+        );
+        res.status(200).json(newCommerce);
+      } else if (resultSearchEmail !== null) {
+        res.status(300).send({ data: "ya existe un comercio con ese email" });
+      } else {
+        res.status(300).send({ data: "ya existe un comercio con ese nombre" });
+      }
     } else {
-      res.status(300).send({ data: "ya existe un camercio con ese nambre" });
-
+      res.status(300).send({ data: "faltan llenar campos" });
     }
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
+
 module.exports = {
   postCommerceHandler,
 };
