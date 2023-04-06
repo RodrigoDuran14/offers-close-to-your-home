48334a07658d1599475e24d295800a6aec7fd677
mateo-mugnaini <mateo.mugnaini.dev@gmail.com>
2023-04-06 13:46:18 -0300

    Footer y correccion de detalle

--- client_users/src/pages/home/Home.jsx
+++ client_users/src/pages/home/Home.jsx
@@ -4,21 +4,28 @@ import Cards from '../../components/cards/Cards'
 import Filter from '../../components/filter/Filter'
 
 import styles from './Home.module.css'
+import Footer from '../../components/footer/Footer'
 
 function Home () {
 
 
   // const productos = useSelector((state) => state.products)
   return (
+    <section>
+
     <div className= {styles.container}>
       <div className={styles.filtros}>
          <Filter/>
       </div>
-
-      <section>
-      <Cards/>
-    </section>
+      <div>
+        <Cards/>
+      </div>
+      <div>
+      <Footer/>
+      </div>
     </div>
+    </section>
+
   )
 }
 
