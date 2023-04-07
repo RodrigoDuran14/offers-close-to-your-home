29304caa1c0cc664370fb84031bb15f4dcf2eb89
Chrisringler <chrisringler@outlook.com.ar>
2023-04-07 18:58:32 -0300

    arreglos en json y creacion de controller motivoCalificacion

--- api/src/routes/producto.js
+++ api/src/routes/producto.js
@@ -4,11 +4,12 @@ const {
   getProductByIdhandler,
 } = require("../handlers/productos/getProductos");
 const { postProductHandler } = require("../handlers/productos/postProductos");
+const {crearCalificacion} = require("../controllers/motivoCalificacion/motivoCalificacionController")
 
 const router = Router();
 
 router.get("/", getAllProductsHandler);
 router.get("/:idProduct", getProductByIdhandler);
 router.post("/", postProductHandler);
-
+router.post("/:idProduct/calificacion", crearCalificacion)
 module.exports = router;
