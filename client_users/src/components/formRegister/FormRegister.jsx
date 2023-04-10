249b0bd507fac30c18bd5ed6916f206828fc687d
fg-francogaleano <fg.francogaleano@gmail.com>
2023-04-10 09:17:06 -0300

    cambios

--- client_users/src/components/formRegister/FormRegister.jsx
+++ client_users/src/components/formRegister/FormRegister.jsx
@@ -22,11 +22,11 @@ export default function FormRegister() {
 
     const handleInputChange = (event) => {
         const property = event.target.name;
-<<<<<<< HEAD
         const value = event.target.value        
-=======
+        setForm({...form, [property] : value})
+    const handleInputChange = (event) => {
+        const property = event.target.name;
         const value = event.target.value
->>>>>>> 90f7d7a9ad4c1fb2317bfe06bcc3d60bb7a47c38
         setForm({...form, [property] : value})
     }
     const [form, setForm] = useState({
@@ -36,66 +36,11 @@ export default function FormRegister() {
         primer_apellido: '',
         segundo_apellido: '',
         direccion: '',
-<<<<<<< HEAD
-        telefono: 0,
-=======
         telefono: '',
->>>>>>> 90f7d7a9ad4c1fb2317bfe06bcc3d60bb7a47c38
         email: '',
         contraseña: '',
         id_ciudad: 0,
         estado: true,
-<<<<<<< HEAD
-       })
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input type="text" name="primer_nombre" value={form.primer_nombre} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Nombre1</label>
-                </div>
-
-                <div>
-                    <input type="text" name="segundo_nombre" value={form.segundo_nombre} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Nombre2</label>
-                </div>
-
-                <div>
-                    <input type="text" name="primer_apellido" value={form.primer_apellido} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Apellido1</label>
-                </div>
-
-                <div>
-                    <input type="text" name="segundo_apellido" value={form.segundo_apellido} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Apellido2</label>
-                </div>
-
-                <div>
-                    <input type="text" name="direccion" value={form.direccion} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Direccion</label>
-                </div>
-
-                <div>
-                    <input type="text" name="telefono" value={form.telefono} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Telefono</label>
-                </div>
-
-                <div>
-                    <input type="text" name="email" value={form.email} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Email</label>
-                </div>
-
-                <div>
-                    <input type="password" name="contraseña" value={form.contraseña} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Contraseña</label>
-                </div>
-
-                <div>
-                    <input type="text" name="id_ciudad" value={form.id_ciudad} onChange={handleInputChange} className={style.input}/>
-                    <label for= "" className={style.label}>Ciudad</label>
-                </div>
-                
-=======
         imagen: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"
        })
        const telefono = typeof(form.telefono)
@@ -130,7 +75,6 @@ export default function FormRegister() {
                 <input type="text" name="id_ciudad" value={form.id_ciudad} onChange={handleInputChange} className={style.input}/>
                 <label for= "" className={style.label}>Ciudad</label>
 
->>>>>>> 90f7d7a9ad4c1fb2317bfe06bcc3d60bb7a47c38
                 <button type="submit" className={style.button}>Registrase</button>
             </form>
         </div>
