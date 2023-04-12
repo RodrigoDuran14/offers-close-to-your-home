3af27ec993eda7d67b4c5eb7ae61f948d86a7baf
fg-francogaleano <fg.francogaleano@gmail.com>
2023-04-12 14:53:44 -0300

    Se dio estilos en componente shoppingcart, falta corregir un error de validación del stock

--- client_users/src/pages/shopping/ShoppingCart.jsx
+++ client_users/src/pages/shopping/ShoppingCart.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
 import CartCard from "../../components/Cart_card/CartCard"
-import s from './shopping.module.css'
+import styles from './shopping.module.css'
 
 export default function ShoppingCart() {
   const carrito = useSelector(state => state.carrito);
@@ -15,6 +15,11 @@ console.log(carrito)
 
 
 console.log(storage_cart);
+  let total = 0
+  carrito.forEach(producto => {
+    total = total + producto.valor_con_descuento*producto.cantidad
+  }); 
+
   return (
     <div>
       <h2>Carrito de compras</h2>
@@ -26,11 +31,22 @@ console.log(storage_cart);
           nombre={producto.nombre} 
           valor_con_descuento={producto.valor_con_descuento}
           cantidad={producto.cantidad}
+          total={total}
           />
         ))
       ) : (
         <p>No hay productos en el carrito.</p>
       )}
+      <div className={styles.containerTotal}>
+        <div className={styles.total}>
+          <div style={{fontSize: "30px", marginLeft: "15px"}}>
+            <h3>Total</h3>
+          </div>
+          <div style={{fontSize: "30px", marginRight: "15px"}}>
+            <h3>${total}</h3>
+          </div>
+        </div>
+      </div>
       <button>Confirmar compra</button>
     </div>
   );
