397734880e698319f0c7bcae28b1fbb6e460f42e
Rodrigo Martin Durán <potro_duran96@hotmail.com>
2023-04-12 13:56:35 -0300

    arreglo de loader con estado global, falta un detalle de estilos

--- client_users/src/components/cards/Cards.jsx
+++ client_users/src/components/cards/Cards.jsx
@@ -1,20 +1,19 @@
-import React, {useEffect, useState}from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../card/Card";
 import styles from "./Cards.module.css";
 import { useSelector, useDispatch } from "react-redux";
 // import {getAllProducts} from '../../redux/actions'
 import Loader from "../loader/loader";
 
-
 const Cards = () => {
-  const { productsFitered } = useSelector((state) => state);
+  const { productsFitered, display } = useSelector((state) => state);
   // const dispatch = useDispatch();
- console.log(productsFitered);
+  console.log(productsFitered);
   // useEffect(() => {
-  //   dispatch(getAllProducts());  
+  //   dispatch(getAllProducts());
   // }, [dispatch]);
 
- // PAGINADO
+  // PAGINADO
 
   const [numeroPagina, setNumeroPagina] = useState(1);
 
@@ -22,7 +21,10 @@ const Cards = () => {
   const conteoFinal = numeroPagina * grupo;
   const conteoInicial = conteoFinal - grupo;
 
-  const aux = productsFitered && productsFitered.slice ? productsFitered.slice(conteoInicial, conteoFinal) : [];
+  const aux =
+    productsFitered && productsFitered.slice
+      ? productsFitered.slice(conteoInicial, conteoFinal)
+      : [];
 
   const paginas = [];
 
@@ -34,50 +36,54 @@ const Cards = () => {
 
   return (
     <div className={styles.container}>
-      <div className={styles.card}>
-      {aux.length <= 0 ? <Loader /> : 
-      aux?.map((product,index) => {
-        return (
-          <Card
-          key={index}
-          producto={product}
-          />
-        )
-      })}
-      </div>
-      <div className={styles.paginado}>
-      <div className= {styles.paginadoAbj}>
-        {/* ------------------------------CONTENEDOR PAGINADO------------------------------ */}
-        <div>
-          {/* ------------------------------BOTON ATRAS------------------------------ */}
-          <button
-            className="btnPag"
-            onClick={() => setNumeroPagina(numeroPagina - 1)}
-            disabled={numeroPagina === 1}
-          >
-            ◄
-          </button>
-          {/* ------------------------------BOTONES PAGINAS------------------------------ */}
-          {paginas.map((pagina) => (
-            <button
-              key={pagina}
-              className={`btnPag ${pagina === numeroPagina ? "active" : ""}`}
-              onClick={() => setNumeroPagina(pagina)}
-            >
-              {pagina}
-            </button>
-          ))}
-          {/* ------------------------------BOTON PROXIMO------------------------------ */}
-          <button
-            className="btnPag"
-            onClick={() => setNumeroPagina(numeroPagina + 1)}
-            disabled={numeroPagina === Math.ceil(productsFitered?.length / grupo)}
-          >
-            ►
-          </button>
-        </div>
-      </div>
-      </div>
+      {display ? (
+        <Loader />
+      ) : (
+        <>
+          <div className={styles.card}>
+            {aux && aux.map((product, index) => {
+              return <Card key={index} producto={product} />;
+            })}
+          </div>
+          <div className={styles.paginado}>
+            <div className={styles.paginadoAbj}>
+              {/* ------------------------------CONTENEDOR PAGINADO------------------------------ */}
+              <div>
+                {/* ------------------------------BOTON ATRAS------------------------------ */}
+                <button
+                  className="btnPag"
+                  onClick={() => setNumeroPagina(numeroPagina - 1)}
+                  disabled={numeroPagina === 1}
+                >
+                  ◄
+                </button>
+                {/* ------------------------------BOTONES PAGINAS------------------------------ */}
+                {paginas.map((pagina) => (
+                  <button
+                    key={pagina}
+                    className={`btnPag ${
+                      pagina === numeroPagina ? "active" : ""
+                    }`}
+                    onClick={() => setNumeroPagina(pagina)}
+                  >
+                    {pagina}
+                  </button>
+                ))}
+                {/* ------------------------------BOTON PROXIMO------------------------------ */}
+                <button
+                  className="btnPag"
+                  onClick={() => setNumeroPagina(numeroPagina + 1)}
+                  disabled={
+                    numeroPagina === Math.ceil(productsFitered?.length / grupo)
+                  }
+                >
+                  ►
+                </button>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
--- client_users/src/pages/shopping/ShoppingCart.jsx
+++ client_users/src/pages/shopping/ShoppingCart.jsx
@@ -1,38 +1,45 @@
-import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
-import CartCard from "../../components/Cart_card/CartCard"
-import s from './shopping.module.css'
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import CartCard from "../../components/Cart_card/CartCard";
+import Loader from "../../components/loader/loader";
+import s from "./shopping.module.css";
 
 export default function ShoppingCart() {
-  const carrito = useSelector(state => state.carrito);
-console.log(carrito)
+  const carrito = useSelector((state) => state.carrito);
+  const display = useSelector((state) => state.display);
+  console.log(carrito);
   useEffect(() => {
-    window.localStorage.setItem('carrito', JSON.stringify(carrito))
-    window.localStorage.getItem('carrito')
-  }, [carrito])
-  
-  const storage_cart = JSON.parse(window.localStorage.getItem('carrito'))
+    window.localStorage.setItem("carrito", JSON.stringify(carrito));
+    window.localStorage.getItem("carrito");
+  }, [carrito]);
 
+  const storage_cart = JSON.parse(window.localStorage.getItem("carrito"));
 
-console.log(storage_cart);
+  console.log(storage_cart);
   return (
-    <div>
-      <h2>Carrito de compras</h2>
-      {carrito && carrito.length > 0 ? (
-        carrito.map(producto => (
-          <CartCard key={producto.id}
-          id_producto = {producto.id_producto}
-          imagen={producto.imagen} 
-          nombre={producto.nombre} 
-          valor_con_descuento={producto.valor_con_descuento}
-          cantidad={producto.cantidad}
-          />
-        ))
+    <>
+      {display ? (
+        <Loader />
       ) : (
-        <p>No hay productos en el carrito.</p>
+        <div>
+          <h2>Carrito de compras</h2>
+          {carrito && carrito.length > 0 ? (
+            carrito.map((producto) => (
+              <CartCard
+                key={producto.id}
+                id_producto={producto.id_producto}
+                imagen={producto.imagen}
+                nombre={producto.nombre}
+                valor_con_descuento={producto.valor_con_descuento}
+                cantidad={producto.cantidad}
+              />
+            ))
+          ) : (
+            <p>No hay productos en el carrito.</p>
+          )}
+          <button>Confirmar compra</button>
+        </div>
       )}
-      <button>Confirmar compra</button>
-    </div>
+    </>
   );
-
 }
